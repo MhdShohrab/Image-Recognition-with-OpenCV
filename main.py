ce9c79ad30f9874afc990faf57f58087dd4311c8
Md Shohrab Ansari <120369657+MhdShohrab@users.noreply.github.com>
2024-04-30 00:33:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 
-
-# pip install opencv-contrib-python # some people ask the difference between this and opencv-python
-                                    # and opencv-python contains the main packages wheras the other
-                                    # contains both main modules and contrib/extra modules
-# pip install cvlib # for object detection
-# # pip install gtts
-# # pip install playsound
-# use `pip3 install PyObjC` if you want playsound to run more efficiently.
-
 import cv2
 import cvlib as cv
 from cvlib.object_detection import draw_bbox
@@ -34,12 +25,7 @@ while True:
     ret, frame = video.read()
     # ret: A boolean indicating whether the frame was successfully captured.
     # frame: The actual image/frame captured from the camera.
-    # Bounding box.
-    # the cvlib library has learned some basic objects using object learning
-    # usually it takes around 800 images for it to learn what a phone is.
-    # if frame is None:
-    #     print("Error reading the frame.")
-    #     break
+
     bbox, label, conf = cv.detect_common_objects(frame)
 
     output_image = draw_bbox(frame, bbox, label, conf)
